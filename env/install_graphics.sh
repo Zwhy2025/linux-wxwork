a88c158ca0995bdf3510915eb4dc81762babcf0f
Zhao Jun <zwhy2025@gmail.com>
2025-08-05 23:11:02 +0800

    fix: 修复企业微信工具栏崩溃问题
    缺少图形显示库

--- env/install_graphics.sh
+++ env/install_graphics.sh
@@ -5,39 +5,19 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -y --no-install-recommends \
-    libglu1-mesa \
-    libglu1-mesa-dev \
-    libopengl0 \
-    libglvnd-dev \
-    libgl1-mesa-dev \
-    libgl1-mesa-glx \
-    libegl1-mesa-dev \
-    libx11-6 \
-    libx11-dev \
-    libx11-xcb1 \
-    libxrandr-dev \
-    libxrandr2 \
-    libxinerama-dev \
-    libxcursor-dev \
-    libxcursor1 \
-    libxi-dev \
-    libxi6 \
-    libxxf86vm-dev \
-    libsm6 \
-    libxext6 \
-    libxrender1 \
-    libxt6 \
-    libxtst6 \
-    libxss1 \
-    libxcomposite1 \
-    libxcb1 \
-    libxcb-xinerama0 \
-    libxcb-icccm4 \
-    libxcb-image0 \
-    libxcb-keysyms1 \
-    libxcb-randr0 \
-    libxcb-shape0 \
-    libxcb-render-util0 \
-    libfreetype6 \
-    libfontconfig1 \
-    libxkbcommon-x11-0 
+    dbus-x11 winbind x11-utils \
+    libfreetype6 libfontconfig1 libvulkan1 \
+    libx11-6 libx11-dev libx11-xcb1 \
+    libxrandr2 libxrandr-dev libxinerama-dev \
+    libxcursor1 libxcursor-dev \
+    libxi6 libxi-dev \
+    libxss1 libsm6 libxt6 libxtst6 \
+    libxext6 libxrender1 libxcomposite1 \
+    libxkbcommon-x11-0 \
+    libxcb1 libxcb-xinerama0 \
+    libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
+    libxcb-randr0 libxcb-shape0 libxcb-render-util0 \
+    libglu1-mesa libglu1-mesa-dev \
+    libgl1-mesa-glx libgl1-mesa-dev \
+    libegl1-mesa-dev libglvnd-dev libopengl0 \
+    libxxf86vm-dev
